6fd1ebc80260cf5a8a1387d9df3c33c1f5d082d1
Igor F. Canduela <ifcanduela@gmail.com>
2016-11-24 14:11:02 +0100

    Default file and folder delete commands

--- SideBarDefaultDisable.py
+++ SideBarDefaultDisable.py
@@ -5,21 +5,21 @@ class NewFileAtCommand(sublime_plugin.WindowCommand):
 		return False
 	def is_enabled(self):
 		return False
-class DeleteFileCommand(sublime_plugin.WindowCommand):
-	def is_visible(self):
-		return False
-	def is_enabled(self):
-		return False
+# class DeleteFileCommand(sublime_plugin.WindowCommand):
+# 	def is_visible(self):
+# 		return False
+# 	def is_enabled(self):
+# 		return False
 class NewFolderCommand(sublime_plugin.WindowCommand):
 	def is_visible(self):
 		return False
 	def is_enabled(self):
 		return False
-class DeleteFolderCommand(sublime_plugin.WindowCommand):
-	def is_visible(self):
-		return False
-	def is_enabled(self):
-		return False
+# class DeleteFolderCommand(sublime_plugin.WindowCommand):
+# 	def is_visible(self):
+# 		return False
+# 	def is_enabled(self):
+# 		return False
 class RenamePathCommand(sublime_plugin.WindowCommand):
 	def is_visible(self):
 		return False
