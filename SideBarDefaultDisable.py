1ec7ed1d01fd633a49aa23ee02e76de202c6dd54
Igor F. Canduela <ifcanduela@gmail.com>
2017-08-21 09:57:20 +0200

    Restore default Sublime New File/New Folder commands

--- SideBarDefaultDisable.py
+++ SideBarDefaultDisable.py
@@ -1,20 +1,20 @@
 from Default.side_bar import *
 
-class NewFileAtCommand(sublime_plugin.WindowCommand):
-	def is_visible(self):
-		return False
-	def is_enabled(self):
-		return False
+# class NewFileAtCommand(sublime_plugin.WindowCommand):
+# 	def is_visible(self):
+# 		return False
+# 	def is_enabled(self):
+# 		return False
 # class DeleteFileCommand(sublime_plugin.WindowCommand):
 # 	def is_visible(self):
 # 		return False
 # 	def is_enabled(self):
 # 		return False
-class NewFolderCommand(sublime_plugin.WindowCommand):
-	def is_visible(self):
-		return False
-	def is_enabled(self):
-		return False
+# class NewFolderCommand(sublime_plugin.WindowCommand):
+# 	def is_visible(self):
+# 		return False
+# 	def is_enabled(self):
+# 		return False
 # class DeleteFolderCommand(sublime_plugin.WindowCommand):
 # 	def is_visible(self):
 # 		return False
